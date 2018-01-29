26318fdccc540cbacf8b35c078abfef2595a9877
cesarsld <jaimes.cesar88@gmail.com>
2018-01-29 18:39:25 +0000

    Added onmouselick attached to game array

--- Drop7_GameMechanic.js
+++ Drop7_GameMechanic.js
@@ -1,53 +1,84 @@
 function GameExecution(gameArray) {
-    var isCombo = true;
-    while (iscombo) {
-        var copyOfArray = Array.from(gameArray);
-        //for loop to find disc procs on each collumn
-        for (i = 0; i < gameArray.length; i++) {
-            var vertDiscCount = gameArray[i].length;
-            for (j = 0; j < vertDiscCount; j++) {
-                if (gameArray[i][j] == vertDiscCount) {
-                    gameArray[i][j] = 0;
-                }
 
+    for (i = 0; i < gameArray.length; i++) {
+        var vertDiscCount = gameArray[i].length;
+        for (j = 0; j < vertDiscCount; j++) {
+            if (gameArray[i][j] == vertDiscCount) {
+                //alert("disc exploded");
+                gameArray[i][j] = 0;
             }
+
         }
-        //forloop to find disc procs on rows
-        for (i = 0; i < gameArray.length; i++) {
-            for (j = 0; j < gameArray.length; j++) {
-                var chain = 0;
-                if (gameArray[j].length >= i + 1) {
-                    chain++;
-                }
-                else {
-                    for (k = j - chain; k < chain; k++) {
-                        if (gameArray[k][i] == chain) {
-                            gameArray[k][i] = 0;
-                        }
-                        chain = 0;
+    }
+    //forloop to find disc procs on rows
+    for (i = 0; i < gameArray.length; i++) {
+        var chain = 0;
+        for (j = 0; j <= gameArray.length; j++) {
+            if (j < gameArray.length && gameArray[j].length >= i + 1) {
+                chain++;
+                //console.log("chain size: " + chain);
+            }
+            else if (chain > 0){
+                for (k = j - chain; k < j; k++) {
+                    if (gameArray[k][i] == chain) {
+                        gameArray[k][i] = 0;
                     }
                 }
-            }
-        } 
-        //for loop to explode surrounding stones
-        for (i = 0; i < gameArray.length; i++) {
-            for (j = 0; j < gameArray[i].length; j++) {
-                if (gameArray[i][j] == 0) {
-                    ExplodeSurroundingStones(gameArray, i, j);
-                }
+                chain = 0;
             }
         }
-        //for loop to remove procced discs ; - 4 loops are needed to 
+    } 
+    //for loop to explode surrounding stones
+    for (i = 0; i < gameArray.length; i++) {
+        for (j = 0; j < gameArray[i].length; j++) {
+            if (gameArray[i][j] == 0) {
+                //ExplodeSurroundingStones(gameArray, i, j);
+            }
+        }
+    }
+    //for loop to remove procced discs ; - 4 loops are needed to 
+    var numsToCheck = 1;
+    while (numsToCheck != 0)
+    {
         for (i = 0; i < gameArray.length; i++) {
             for (j = 0; j < gameArray[i].length; j++) {
                 if (gameArray[i][j] == 0) {
                     gameArray[i].splice(j, 1);
+                    numsToCheck = 2;
                 }
             }
         }
-        if (gameArray == copyOfArray) isCombo = false;
+        numsToCheck--;
     }
     return gameArray;
+}
+
+functoin deepCopyArray()
+
+function arraysEqual(arr1, arr2) {
+    console.log ("equal method accessed");
+    console.log("copy check");
+            for (i = 0 ; i < arr2.length ; i++){
+                for (j = 0 ; j < arr2[i].length ; j++){
+                    console.log (arr2[i][j]);
+                }
+            }
+            console.log("array check");
+            for (i = 0 ; i < arr1.length ; i++){
+                for (j = 0 ; j < arr1[i].length ; j++){
+                    console.log (arr1[i][j]);
+                }
+            }
+    if(arr1.length != arr2.length)
+        return false;
+    for (i = 0 ; i < arr1.length ; i++) {
+        for (j = 0 ; j < arr1[i].length ; j ++) {
+            //console.log("game arr = " + arr1[i][j] + "; coppy arr = " + arr2[i][j]);
+            if (arr1[i][j] != arr2[i][j]) return false;
+        }
+    }
+
+    return true;
 }
 
 function ExplodeSurroundingStones(gameArray, posX, posY) {
@@ -96,7 +127,7 @@ function ExplodeSurroundingStones(gameArray, posX, posY) {
         }
     }
     if (posX == 0) {
-        if (gameArray[x + 1].length - 1 >= posY) {
+        if (gameArray[posX + 1].length - 1 >= posY) {
             if (gameArray[posX + 1][posY] < 0) {
                 gameArray[posX + 1][posY]++;
                 if (gameArray[posX + 1][posY] == 0) {
@@ -105,7 +136,7 @@ function ExplodeSurroundingStones(gameArray, posX, posY) {
             }
         }
     }
-    else if (posX > 0 && poX < gameArray.length)
+    else if (posX > 0 && posX < gameArray.length)
     {
         if (gameArray[posX - 1].length - 1 >= posY) {
             if (gameArray[posX - 1][posY] < 0) {
